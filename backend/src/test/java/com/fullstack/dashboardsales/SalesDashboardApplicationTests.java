e9738a2705e639e645bd8e9645cf9e6301f57264
Arthur Kou <arthur_kou@hotmail.com>
2021-05-03 22:39:12 -0300

    project created

---
+++ backend/src/test/java/com/fullstack/dashboardsales/SalesDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.dashboardsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

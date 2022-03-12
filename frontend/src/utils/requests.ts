edb361cd86e4801ac9fe802b43a2a8538f9c3159
arthurkou <arthur_kou@hotmail.com>
2022-03-12 16:53:20 -0300

    set base_url

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,2 @@
 // variable defined at netlify to access locally/production 
-export const BASE_URL = process.env.BACKEND_URL ?? 'http://localhost:8000';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8000';

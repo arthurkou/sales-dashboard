6824a8da5ffffee1632864b8ca6b4c1e68085e8a
arthurkou <arthur_kou@hotmail.com>
2022-03-12 18:49:16 -0300

    ajuste pagination

--- frontend/src/components/pagination/index.tsx
+++ frontend/src/components/pagination/index.tsx
@@ -16,7 +16,7 @@ const Pagination = ( { page, onPageChange } : Props) => {
                         <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
                     </li>
                     <li className="page-item disabled">
-                        <span className="page-link">{page.number}</span>
+                        <span className="page-link">{page.number+1}</span>
                     </li>
                     <li className={`page-item ${page.last ? 'disabled' : ''} `}>
                         <button className="page-link"  onClick={() => onPageChange(page.number + 1)}>Próxima</button>

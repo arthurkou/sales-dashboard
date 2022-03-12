0272138003380dcd8c34fd33c6f6ac120e9c29df
arthurkou <arthur_kou@hotmail.com>
2022-03-12 17:01:51 -0300

    cross origin

--- backend/src/main/java/com/fullstack/sales/controller/SaleController.java
+++ backend/src/main/java/com/fullstack/sales/controller/SaleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.fullstack.sales.dto.SaleSumDTO;
 import com.fullstack.sales.service.SaleService;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping(value = "/sales")
 public class SaleController {
 	
--- backend/src/main/java/com/fullstack/sales/controller/SellerController.java
+++ backend/src/main/java/com/fullstack/sales/controller/SellerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.fullstack.sales.dto.SellerDTO;
 import com.fullstack.sales.service.SellerService;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping(value = "/sellers")
 public class SellerController {
 	

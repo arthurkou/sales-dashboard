25274a2b080fe83e0ebc43ff5a7f66bf857a8e90
arthurkou <arthur_kou@hotmail.com>
2022-03-13 17:27:22 -0300

    remove crossorigin

--- backend/src/main/java/com/fullstack/sales/controller/SaleController.java
+++ backend/src/main/java/com/fullstack/sales/controller/SaleController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,6 @@ import com.fullstack.sales.dto.SaleSumDTO;
 import com.fullstack.sales.service.SaleService;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping(value = "/sales")
 public class SaleController {
 	
